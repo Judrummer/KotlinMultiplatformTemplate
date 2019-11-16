4eafdfa2817ad77c5dda7e72c0c585ddd3c4be11
Tipatai Puthanukunkit <judrummercpe@gmail.com>
2019-11-17 03:52:56 +0700

    initial kotlin core module

---
+++ core/src/commonMain/kotlin/com/kotlinmultiplatformtemplate/core/Sample.kt
@@ -0,0 +1,19 @@
+package com.kotlinmultiplatformtemplate.core
+
+expect class Sample() {
+    fun checkMe(): Int
+}
+
+expect object Platform {
+    val name: String
+}
+
+fun hello(): String = "Hello from ${Platform.name}"
+
+class Proxy {
+    fun proxyHello() = hello()
+}
+
+fun main() {
+    println(hello())
+}
---
+++ core/src/test/java/com/kotlinmultiplatformtemplate/core/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.kotlinmultiplatformtemplate.core
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertTrue { 4 == 2 + 2 }
+    }
+}
