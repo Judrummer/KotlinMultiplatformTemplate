557ba6b235f0a12eb2a794a35e1e21af2b25473b
Tipatai Puthanukunkit <judrummercpe@gmail.com>
2019-11-17 03:32:24 +0700

    init iOS project

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by judrummer on 17/11/2562 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
